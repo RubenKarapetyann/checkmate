1b45bb891d134b83a9b5af435dfd396dd17c635f
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-07-26 01:05:00 +0400

    created component Row with its types and styles

--- client/src/types/game/component-types.ts
+++ client/src/types/game/component-types.ts
@@ -1,9 +1,18 @@
 import { DARK_CELL, LIGHT_CELL } from "../../constants/colors"
+import { Matrix, Row } from "./game" 
+
+type CellHandle = (row: number, column: number) => void
 
 export type CellProps = {
     color : typeof LIGHT_CELL | typeof DARK_CELL,
     row : number,
     column : number,
-    handle : (row: number, column: number) => void,
+    handle : CellHandle,
     figure : string
+}
+
+export type RowProps = {
+    list : Row,
+    row : number,
+    handle : CellHandle
 }
