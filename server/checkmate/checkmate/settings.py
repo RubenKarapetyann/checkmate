908d1d598a54a826b8f4b56a17581c2c6e85c569
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-07-30 02:04:05 +0400

    connected socket to main application

--- server/checkmate/checkmate/settings.py
+++ server/checkmate/checkmate/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,7 +79,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'checkmate.wsgi.application'
-
+ASGI_APPLICATION = 'checkmate.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
