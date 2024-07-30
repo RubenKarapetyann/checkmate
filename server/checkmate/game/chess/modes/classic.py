5f09302b60ac68ecac268849ac8b9ee0b5179afe
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-07-30 14:39:25 +0400

    created constant of the classic mode

---
+++ server/checkmate/game/chess/modes/classic.py
@@ -0,0 +1,13 @@
+NAME = "classic"
+DISPLAY_NAME = "Classic"
+DURATION = 600 # seconds
+LEVEL_AVAILABLE = 1
+ID = 1
+
+CLASSIC = {
+    "name" : NAME,
+    "display_name" : DISPLAY_NAME,
+    "duration" : DURATION,
+    "level_available" : LEVEL_AVAILABLE,
+    "id" : ID
+}
