0ad7b689ada83cabbec9888a69303e0c641be64e
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-04 16:03:18 +0400

    created functions that find horizontal, vertical and diagonal moves

--- server/checkmate/game/chess/figures/base.py
+++ server/checkmate/game/chess/figures/base.py
@@ -19,3 +19,28 @@ class FigureBase:
     
     def get_moves(self):
         pass
+    
+    def get_horizontal_moves(self):
+        return (
+            [ [self.row, self.column + i] for i in range(1, self.m_columns - self.column) ]
+            +
+            [ [self.row, self.column - i] for i in range(1, self.column + 1) ]
+        )
+
+    def get_vertical_moves(self):
+        return (
+            [ [self.row + i, self.column] for i in range(1, self.m_rows - self.row) ]
+            +
+            [ [self.row - i, self.column] for i in range(1, self.row + 1) ]
+        )
+
+    def get_diagonal_moves(self): 
+        return (
+            [ [ self.row + i, self.column + i ] for i in range(1, min(self.m_rows - self.row, self.m_columns - self.column)) ]
+            +
+            [ [ self.row - i, self.column + i ] for i in range(1, min(self.row + 1, self.m_columns - self.column)) ]
+            +
+            [ [ self.row - i, self.column - i ] for i in range(1, min(self.row, self.column) + 1) ]
+            +
+            [ [ self.row + i, self.column - i ] for i in range(1, min(self.m_rows - self.row, self.column + 1)) ]
+        )
