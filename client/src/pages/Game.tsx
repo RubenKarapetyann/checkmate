a8a18e7c5a45e559050f0fe67c0386b42a9b4fb5
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-06 15:26:26 +0400

    used the useSocket hook on the game and lobby pages, now after receiving the game ID the user will be redirected to the game page, receive the matrix and draw it

--- client/src/pages/Game.tsx
+++ client/src/pages/Game.tsx
@@ -1,3 +1,31 @@
+import { useLocation } from "react-router-dom"
+import useSocket from "../hooks/socket/useSocket";
+import Board from "../components/game/Board/Board";
+import { useEffect, useState } from "react";
+import { Matrix } from "../types/game/game";
+import { GAME_ACCEPTED } from "../constants/actions";
+import { GameAcceptedData } from "../types/api/socket";
+import { CellHandle } from "../types/game/component-types";
+
 export default function Game(){
-    return <div>game</div>
+    const location = useLocation()
+    const [matrix, setMatrix] = useState<Matrix | null>(null)
+
+    const { socket, listen } = useSocket("game", location.state.game_id)
+
+    useEffect(()=>{
+        listen<GameAcceptedData>(GAME_ACCEPTED, (data)=>[
+            setMatrix(data.matrix)
+        ])
+    }, [socket])
+
+    if(!matrix){
+        return <div>loading</div>
+    }
+
+    const handle: CellHandle = (row, column)=>{
+        console.log(row, column)
+    }
+
+    return <Board matrix={matrix} handle={handle}/>
 }
