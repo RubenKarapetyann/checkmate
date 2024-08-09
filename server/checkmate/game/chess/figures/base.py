703b9343f28dd89ae89400449726c747950f9a31
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-09 19:04:44 +0400

    created a function for checking and selecting only possible moves and used them in functions for getting moves

--- server/checkmate/game/chess/figures/base.py
+++ server/checkmate/game/chess/figures/base.py
@@ -15,8 +15,12 @@ class FigureBase:
         self.m_rows = len(self.matrix)
         self.m_columns = len(self.matrix[0])
         self.id = f"{uuid.uuid4()}"
+        self.moves = []
         
     def move(self):
+        pass
+    
+    def get_verified_moves(self):
         pass
     
     def eat(self):
@@ -25,27 +29,61 @@ class FigureBase:
     def get_moves(self):
         pass
     
-    def get_horizontal_moves(self):
+    def get_line_moves(self, diapason: int, define_cell):
+        moves = []
+        for i in range(1, diapason):
+            cell = define_cell(self.row, self.column, i, self.matrix)
+            if(cell == 0):
+                moves.append([cell.row, cell.column])
+                continue
+            elif(cell.color == self.color):
+                break
+            else:
+                moves.append([cell.row, cell.column])
+                break
+        
+        return moves
+            
+    def get_horizontal_moves(self):    
         return (
-            [ [self.row, self.column + i] for i in range(1, self.m_columns - self.column) ]
+            self.get_line_moves(self.m_columns - self.column, lambda row, column, i, matrix: matrix[row][column + i])
             +
-            [ [self.row, self.column - i] for i in range(1, self.column + 1) ]
+            self.get_line_moves(self.column + 1, lambda row, column, i, matrix: matrix[row][column - i])
         )
 
     def get_vertical_moves(self):
         return (
-            [ [self.row + i, self.column] for i in range(1, self.m_rows - self.row) ]
+            self.get_line_moves(self.m_rows - self.row, lambda row, column, i, matrix: matrix[row + i][column])
             +
-            [ [self.row - i, self.column] for i in range(1, self.row + 1) ]
+            self.get_line_moves(self.row + 1, lambda row, column, i, matrix: matrix[row - i][column])
         )
 
     def get_diagonal_moves(self): 
         return (
-            [ [ self.row + i, self.column + i ] for i in range(1, min(self.m_rows - self.row, self.m_columns - self.column)) ]
+            self.get_line_moves(min(self.m_rows - self.row, self.m_columns - self.column), lambda row, column, i, matrix: matrix[row + i][column + i])
             +
-            [ [ self.row - i, self.column + i ] for i in range(1, min(self.row + 1, self.m_columns - self.column)) ]
+            self.get_line_moves(min(self.row + 1, self.m_columns - self.column), lambda row, column, i, matrix: matrix[row - i][column + i])
             +
-            [ [ self.row - i, self.column - i ] for i in range(1, min(self.row, self.column) + 1) ]
+            self.get_line_moves(min(self.row, self.column) + 1, lambda row, column, i, matrix: matrix[row - i][column - i])
             +
-            [ [ self.row + i, self.column - i ] for i in range(1, min(self.m_rows - self.row, self.column + 1)) ]
+            self.get_line_moves(min(self.m_rows - self.row, self.column + 1), lambda row, column, i, matrix: matrix[row + i][column - i])
         )
+        
+    def confirm_moves(self):
+        moves = self.moves
+        matrix = self.matrix
+        matrix_len = len(self.matrix) - 1
+        verified_moves = []
+        
+        for move in moves:
+            if move[0] < 0 or move[0] > matrix_len or move[1] < 0 or move[1] > matrix_len:
+                continue
+            
+            cell = matrix[move[0]][move[1]]
+            if cell != 0 and cell.color == self.color:
+                continue
+            
+            verified_moves.append(move)
+            
+        return verified_moves
+        
