9cf756d9a6c275a01c67728bd6812d549e697685
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-09 18:57:54 +0400

    the matrix serializer was modified, a serializer was created to convert the object matrix back to the class matrix, and a bug was fixed

--- server/checkmate/game/wbserializers.py
+++ server/checkmate/game/wbserializers.py
@@ -1,4 +1,5 @@
 from json import JSONEncoder
+from .chess.figures.layout import figures
 
 class MatrixSerializer(JSONEncoder):
     def default(self, figure):
@@ -7,5 +8,17 @@ class MatrixSerializer(JSONEncoder):
             "id" : dict["id"],
             "image" : dict["image"],
             "color" : dict["color"],
-            "number" : dict["number"]
+            "number" : dict["number"],
+            "row" : dict["row"],
+            "column" : dict["column"]
         }
+        
+
+def from_db_objects_to_classes_serializer(json_object):
+    if "number" in json_object:
+        return figures[json_object["number"] - 1](
+            row=json_object["row"],
+            column=json_object["column"],
+            matrix=[[]],
+            color=json_object["color"]
+        )
