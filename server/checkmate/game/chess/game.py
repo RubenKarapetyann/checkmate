b40c48635e06092189a512d962dbdd71622307b3
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-13 21:04:47 +0400

    created the constructor of the game class

---
+++ server/checkmate/game/chess/game.py
@@ -0,0 +1,7 @@
+class Chess:
+    def __init__(self, moves_count, mode, matrix, white_id, black_id):
+        self.moves_count = moves_count
+        self.mode = mode
+        self.matrix = matrix
+        self.white_id = white_id
+        self.black_id = black_id
