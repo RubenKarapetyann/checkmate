fb46976b5346756ae83cf6df62d63d511968ab37
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-19 12:15:38 +0400

    created constants of the game results and states and constant of the action 'game finished'

--- client/src/constants/game.ts
+++ client/src/constants/game.ts
@@ -1,3 +1,15 @@
 export const MATRIX_SIZE = 8
 export const BLACK = "black"
 export const WHITE = "white"
+
+
+// states
+export const CHECK = "check"
+export const CHECKMATE = "checkmate"
+export const STALEMATE = "stalemate"
+export const REGULAR = "regular"
+
+// results
+export const WIN = "win"
+export const LOSE = "lose"
+export const DRAW = "draw"
