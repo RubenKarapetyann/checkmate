e1fdb5dd90a2cdac192830f9ae88c1006a19a9c4
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-22 23:41:12 +0400

    added styles for the component GameHeaderLink

--- client/src/components/links/GameHeaderLink/GameHeaderLink.tsx
+++ client/src/components/links/GameHeaderLink/GameHeaderLink.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom"
 import { GameHeaderLinkProps } from "../../../types/components/links"
+import styles from "./GameHeaderLink.module.scss"
 
 const GameHeaderLink = ({ path, label, icon }: GameHeaderLinkProps)=>{
     return (
         <NavLink 
             to={path} 
+            className={styles.link}
         >
             <div>
                 <img src={icon} alt={label}/>
