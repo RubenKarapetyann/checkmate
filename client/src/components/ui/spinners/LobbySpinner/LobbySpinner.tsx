110c43b6853265ea4632fbf15e96ff742b27ef22
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-23 16:13:54 +0400

    added the lobby spinner component

---
+++ client/src/components/ui/spinners/LobbySpinner/LobbySpinner.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material"
+
+const LobbySpinner = ()=>{
+    return (
+        <Box>
+            <img 
+                src="images/spinners/board2x2.svg" 
+                alt="spinner" 
+                width="150px"
+            />
+        </Box>
+    )
+}
+
+export default LobbySpinner
