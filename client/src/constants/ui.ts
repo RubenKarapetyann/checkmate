3921832e2659b17d2ebda3c5cebe5bf8bd3609ae
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-23 16:58:37 +0400

    created phrases for lobby

---
+++ client/src/constants/ui.ts
@@ -0,0 +1,16 @@
+export const LOBBY_PHRASES = [
+    "Know the value and movement of each chess piece.",
+    "Understand the rules of the game, including capturing, check, checkmate, and stalemate.",
+    "Work on tactical puzzles to improve your ability to spot and exploit tactical opportunities.",
+    "The center of the board is a strategic advantage. Try to control it with your pieces.",
+    "The more you play, the better you'll become.",
+    "Learn popular chess openings and their variations.",
+    "Think ahead a few moves.",
+    "Analyze your games.",
+    "Study grandmaster games.",
+    "Have fun!",
+    "Learn from your mistakes.",
+    "Don't rush your queen out too early.",
+    "Control the diagonals.",
+    "Be aware of your pawn structure."
+]
