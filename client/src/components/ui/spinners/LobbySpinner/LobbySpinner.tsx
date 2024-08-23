039cadd0761693c76c0cdf52683719c7592ec7bf
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-23 16:16:56 +0400

    added styles and animation for the lobby spinner

--- client/src/components/ui/spinners/LobbySpinner/LobbySpinner.tsx
+++ client/src/components/ui/spinners/LobbySpinner/LobbySpinner.tsx
@@ -1,8 +1,9 @@
 import { Box } from "@mui/material"
+import styles from "./LobbySpinner.module.scss"
 
 const LobbySpinner = ()=>{
     return (
-        <Box>
+        <Box className={styles.container}>
             <img 
                 src="images/spinners/board2x2.svg" 
                 alt="spinner" 
