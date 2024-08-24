deee11d3c4657d2bd5c2c1f8576e6d65374df230
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-24 14:23:40 +0400

    created an animated clock

---
+++ client/src/components/ui/details/AnimatedClock/AnimatedClock.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material"
+import styles from "./AnimatedClock.module.scss"
+
+const AnimatedClock = ()=>(
+    <Box className={styles.container}>
+        <div></div>
+    </Box>
+)
+
+export default AnimatedClock
