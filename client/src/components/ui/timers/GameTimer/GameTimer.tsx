e237bde30644525ca0acd2c838929b9d9b9f5aa3
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-24 16:22:25 +0400

    added styles for the game timer

--- client/src/components/ui/timers/GameTimer/GameTimer.tsx
+++ client/src/components/ui/timers/GameTimer/GameTimer.tsx
@@ -2,13 +2,14 @@ import { Box } from "@mui/material"
 import { GameTimerProps } from "../../../../types/components/timers"
 import AnimatedClock from "../../details/AnimatedClock/AnimatedClock"
 import { useState } from "react"
+import styles from "./GameTimer.module.scss"
 
 const GameTimer = ({ time, onFinish }: GameTimerProps) => {
     const [seconds, setSeconds] = useState<number>(time)
     const minutes = Math.floor(seconds / 60)
 
     return (
-        <Box>
+        <Box className={styles.container}>
             <AnimatedClock/>
             <p>{minutes} : {seconds - minutes * 60}</p>
         </Box>
