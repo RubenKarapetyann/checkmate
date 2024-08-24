3d6542a6900334d5b40d41eec08df1664a265408
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-24 16:24:23 +0400

    added functionality for the game timer

--- client/src/components/ui/timers/GameTimer/GameTimer.tsx
+++ client/src/components/ui/timers/GameTimer/GameTimer.tsx
@@ -1,12 +1,28 @@
 import { Box } from "@mui/material"
 import { GameTimerProps } from "../../../../types/components/timers"
 import AnimatedClock from "../../details/AnimatedClock/AnimatedClock"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from "./GameTimer.module.scss"
 
 const GameTimer = ({ time, onFinish }: GameTimerProps) => {
     const [seconds, setSeconds] = useState<number>(time)
     const minutes = Math.floor(seconds / 60)
+
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{
+            setSeconds(seconds=>{
+                if( seconds > 0 ){
+                    return seconds - 1
+                }else{
+                    clearInterval(intervalId)
+                    onFinish()
+                    return 0
+                }
+            })                
+        }, 1000)
+
+        return ()=> clearInterval(intervalId)
+    }, [])
 
     return (
         <Box className={styles.container}>
