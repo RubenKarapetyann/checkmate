48432aed95e00ee7d68b6978b224013b4b372e60
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-25 20:54:35 +0400

    added simple styles for the game layout and modified global styles

--- client/src/components/layouts/GameLayout/GameLayout.tsx
+++ client/src/components/layouts/GameLayout/GameLayout.tsx
@@ -1,12 +1,26 @@
-import { Container } from "@mui/material"
+import { Box, Container } from "@mui/material"
 import { GameLayoutProps } from "../../../types/components/layouts"
 import FunctionalBoard from "../../game/FunctionalBoard/FunctionalBoard"
+import styles from "./GameLayout.module.scss"
+import GameTimer from "../../ui/timers/GameTimer/GameTimer"
 
 const GameLayout = ({ send }: GameLayoutProps)=>{
 
     return (
         <Container>
-            <FunctionalBoard send={send}/>
+            <Box className={styles.container}>
+                <Box>
+                    <FunctionalBoard send={send}/>
+                </Box>
+                <Box className={styles.statistics}>
+                    <Box>
+                        <GameTimer time={150} onFinish={()=>{}}/>
+                    </Box>
+                    <Box>
+                        <GameTimer time={150} onFinish={()=>{}}/>
+                    </Box>
+                </Box>
+            </Box>
         </Container>
     )
 }
