557a46666d3f4605cd6834931974f889e2417953
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-08-27 18:10:53 +0400

    created an api for getting authenticated user's information

--- server/checkmate/users/constants.py
+++ server/checkmate/users/constants.py
@@ -1,5 +1,9 @@
 from checkmate.constants import USERS
+from classes.routing import AppConstant
+from .views import UserAPIView
 
 APP_NAME = USERS.namespace
 
-routes = []
+USER = AppConstant("user/", UserAPIView.as_view(), "user")
+
+routes = [USER]
--- server/checkmate/users/views.py
+++ server/checkmate/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserAPIView(APIView):
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+     
+    def get(self, request):
+        user = UserSerializer(request.user).data
+        return Response({ "user" : user })
